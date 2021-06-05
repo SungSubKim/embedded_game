e08a2197b8b381ab4edb51ab5dffa19ccdcd59b1
chizz <bang627@skku.edu>
2021-06-05 17:52:47 +0900

    [Add] background for each level

---
+++ background.h
@@ -0,0 +1,6 @@
+vector<const unsigned char *> background;
+background.push_back(level0);
+background.push_back(level1);
+background.push_back(level2);
+background.push_back(level3);
+background.push_back(level4);

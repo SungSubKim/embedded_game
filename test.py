2474108bfc2b0ff9cb853adec91166ca2b1b8fb7
devquint <devquint@noreply.github.com>
2021-06-06 20:00:33 +0900

    [UPDATE] update driver-using python example code

--- test.py
+++ test.py
@@ -6,4 +6,6 @@ if __name__ == "__main__":
             time.sleep(0.1)
             ar = array.array('L', [0,0,0,0])
             fcntl.ioctl(rpikey, 100, ar, 1)
+            led = array.array('L', [1^ar[0], 1^ar[1], 1^ar[2]])
+            fcntl.ioctl(rpikey, 101, led, 0)
             print(ar)
